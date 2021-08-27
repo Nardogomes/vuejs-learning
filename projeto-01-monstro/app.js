b081e3889c5bbddbfbc50ff459b1e2263d27033d
Ednardo Gomes <ednardo@email.com>
2021-08-27 19:39:41 -0300

    Finalizando o painel de resultado.

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -1,18 +1,14 @@
 new Vue({
-  el: '#app',
+  el: "#app",
   data: {
     playerLife: 100,
-    monsterLife: 0
+    monsterLife: 0,
   },
   computed: {
     hasResult() {
-      return this.playerLife == 0 || this.monsterLife == 0
-    }
+      return this.playerLife == 0 || this.monsterLife == 0;
+    },
   },
-  methods: {
-
-  },
-  watch: {
-    
-  }
+  methods: {},
+  watch: {},
 });
