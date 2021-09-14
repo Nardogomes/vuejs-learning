940818f979e1df81dae0baa0f42b8809597fc0fd
Ednardo Gomes <ednardo@gomes.com>
2021-09-14 17:45:44 -0300

    Comunicação entre componentes irmãos.

--- comunicacao/src/components/Usuario.vue
+++ comunicacao/src/components/Usuario.vue
@@ -3,15 +3,21 @@
     <h1>Componente Usuário</h1>
     <p>Esse é um componente muito legal!</p>
     <p>Nome é <strong>{{ nome }}</strong></p>
+    <p>Idade é <strong>{{ idade }}</strong></p>
     <button @click="alterarNome">Alterar Nome</button>
     <hr />
     <div class="componentes">
       <app-usuario-info
         :nome="nome"
+        :idade="idade"
         @nomeMudou="nome = $event"
         :reiniciarFn="reiniciarNome"
       />
-      <app-usuario-editar />
+      <app-usuario-editar
+        :idade="idade"
+        @idadeMudou="idade = $event"
+        :reiniciarFn="reiniciarIdade"
+      />
     </div>
   </div>
 </template>
@@ -25,6 +31,7 @@ export default {
   data() {
     return {
       nome: "Nardo",
+      idade: 30
     };
   },
   methods: {
@@ -33,6 +40,8 @@ export default {
     },
     reiniciarNome() {
       this.nome = 'Nardo'
+    },reiniciarIdade() {
+      this.idade = 30
     }
   },
 };
--- comunicacao/src/components/UsuarioEditar.vue
+++ comunicacao/src/components/UsuarioEditar.vue
@@ -2,12 +2,21 @@
     <div class="componente">
         <h2>Alterar os Dados de Usuário</h2>
         <p>Edite as informações</p>
+        <p>Idade do usuário: <strong>{{ idade }}</strong></p>
+        <button @click="alterarIdade">Alterar Idade</button>
+        <button @click="reiniciarFn">Reiniciar Idade</button>
     </div>
 </template>
 
 <script>
 export default {
-    
+  props: ['idade', 'reiniciarFn'],
+  methods: {
+    alterarIdade() {
+      // this.idade = 33
+      this.$emit('idadeMudou', 33)
+    }
+  }
 }
 </script>
 
