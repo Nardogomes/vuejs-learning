fb6a6aea2eaff230b29138b7cc90e5b124df57ab
Ednardo Gomes <ednardo@gomes.com>
2021-10-08 15:22:32 -0300

    Registrando diretiva de forma local.

--- diretivas/diretivas-exercicios/src/App.vue
+++ diretivas/diretivas-exercicios/src/App.vue
@@ -7,6 +7,9 @@
 		<hr>
 		<p v-destaque:fundo.atrasar="'lightblue'">Usando diretiva personalizada.</p>
 		<p v-destaque="color">Usando diretiva personalizada.</p>
+		<hr>
+		<p v-destaque-local:fundo.atrasar="'lightblue'">Usando diretiva local personalizada.</p>
+		<p v-destaque-local="color">Usando diretiva local personalizada.</p>
 
 		<!-- <p v-teste:argumento.mod1.mod2.mod3="'valor'"></p> -->
 	
@@ -15,6 +18,24 @@
 
 <script>
 export default {
+	directives: {
+		'destaque-local': {
+			bind(el, binding, vnode) {
+				// el.style.backgroundColor = 'lightgreen'
+				
+				let atraso = 0
+				if (binding.modifiers['atrasar']) atraso = 3000
+
+				setTimeout(() => {
+					if (binding.arg == 'fundo') {
+						el.style.backgroundColor = binding.value
+					} else {
+						el.style.color = binding.value
+					}
+				}, atraso)
+			}
+		}
+	},
 	data() {
 		return {
 			color: 'red'
