2e9b97863b995e4a3d5b37e01a112e364f3c77f3
Ednardo Gomes <ednardo@gomes.com>
2021-11-18 17:12:13 -0300

    configurando o axios.

---
+++ http/http-exercicios/src/plugins/axios.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://vue-http-7d581-default-rtdb.firebaseio.com/'
+
+Vue.use({
+  install(Vue) {
+    Vue.prototype.$http = axios
+  }
+})
